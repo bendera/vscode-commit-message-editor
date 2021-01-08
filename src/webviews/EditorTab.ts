34919814812eac2d01cd6e95d5e05f18c181d5d8
bendera <szoftverhiba@gmail.com>
2021-01-08 23:13:17 +0100

    build: optimize package size

--- src/webviews/EditorTab.ts
+++ src/webviews/EditorTab.ts
@@ -2,8 +2,6 @@ import * as path from 'path';
 import * as vscode from 'vscode';
 import getNonce from '../utils/getNonce';
 
-type VisibleViewsConfig = 'text' | 'form' | 'both';
-
 interface EditorTabProps {
   extensionPath: string;
   platform: string;
@@ -13,7 +11,7 @@ interface EditorTabProps {
   saveAndClose: boolean;
 }
 
-const EditorTab = ({ extensionPath, platform, webview, defaultView, showRecentCommits, saveAndClose }: EditorTabProps) => {
+const EditorTab = ({ extensionPath, platform, webview }: EditorTabProps) => {
   const assetUri = (fp: string) => {
     const fragments = fp.split('/');
     const vscodeUri = vscode.Uri.file(
@@ -23,87 +21,8 @@ const EditorTab = ({ extensionPath, platform, webview, defaultView, showRecentCo
     return webview.asWebviewUri(vscodeUri);
   };
 
-  const config = vscode.workspace.getConfiguration('commit-message-editor');
-  const visibleViews: VisibleViewsConfig = config.get('view.visibleViews') || 'both';
   const { cspSource } = webview;
   const nonce = getNonce();
-  const selectedTabIndex = defaultView === 'text' ? 0 : 1;
-
-  let recentCommitsTemplate = '';
-
-  if (showRecentCommits) {
-    recentCommitsTemplate = `
-      <div class="recent-commits">
-        <h2 class="recent-commits__title">Recent commits:</h2>
-        <div id="recent-commits-wrapper" class="recent-commits-wrapper is-loading">
-          <div id="recent-commits-wrapper__loading" class="recent-commits-wrapper__loading">
-            <div class="recent-commits-wrapper__loading-icon"></div>
-          </div>
-          <div id="recent-commits-wrapper__commits" class="recent-commits-wrapper__commits">
-            <vscode-tree id="recent-commits-wrapper__commits-list" tabindex="0"></vscode-tree>
-          </div>
-        </div>
-      </div>
-    `;
-  }
-
-  const textareaTabHeader = `<header slot="header">Edit as text</header>`;
-
-  const textareaTabContent = `
-    <section>
-      <div class="editor-toolbar">
-        <p><a href="#" title="Load configured template" id="load-template-button"><vscode-icon name="file"></vscode-icon>Load template</a></p>
-      </div>
-      <vscode-inputbox
-        multiline="true"
-        id="message-box"
-        lines="10"
-        maxlines="20"
-      ></vscode-inputbox>
-      <div class="buttons">
-        <vscode-button id="success-button-text">${saveAndClose ? 'Save and close' : 'Save'}</vscode-button>
-        <vscode-button id="cancel-button-text">Cancel</vscode-button>
-        <vscode-checkbox
-          label="Amend previous commit"
-          class="cb-amend"
-          id="text-amend-checkbox"
-        ></vscode-checkbox>
-      </div>
-      ${recentCommitsTemplate}
-    </section>
-  `;
-
-  const formTabHeader = `<header slot="header">Edit as form</header>`;
-
-  const formTabContent = `
-    <section>
-      <div id="edit-form"></div>
-      <div class="buttons">
-        <vscode-button id="success-button-form">${saveAndClose ? 'Save and close' : 'Save'}</vscode-button>
-        <vscode-button id="cancel-button-form">Cancel</vscode-button>
-        <vscode-checkbox
-          label="Amend previous commit"
-          class="cb-amend"
-          id="form-amend-checkbox"
-        ></vscode-checkbox>
-      </div>
-    </section>
-  `;
-
-  let content = '';
-
-  if (visibleViews === 'form') {
-    content += formTabContent;
-  } else if (visibleViews === 'text') {
-    content += textareaTabContent;
-  } else {
-    content += `<vscode-tabs id="main-tabs" selectedindex="${selectedTabIndex}">`;
-    content += textareaTabHeader;
-    content += textareaTabContent;
-    content += formTabHeader;
-    content += formTabContent;
-    content += '</vscode-tabs>'
-  }
 
   const html = `
     <!DOCTYPE html>
@@ -119,13 +38,7 @@ const EditorTab = ({ extensionPath, platform, webview, defaultView, showRecentCo
       <link rel="stylesheet" href="${assetUri('node_modules/vscode-codicons/dist/codicon.css')}" nonce="${nonce}" id="vscode-codicon-stylesheet">
     </head>
     <body class="${platform}">
-      <div class="content">
-        <div class="layout">
-          <div class="col">
-            <cme-editor-page></cme-editor-page>
-          </div>
-        </div>
-      </div>
+      <cme-editor-page></cme-editor-page>
       <script src="${assetUri('frontend/dist/bundled.js')}" nonce="${nonce}" type="module"></script>
     </body>
     </html>
