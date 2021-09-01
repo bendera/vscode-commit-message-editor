171934b47a7c4777f931616aa116ef3b00211ead
bendera <szoftverhiba@gmail.com>
2021-09-01 21:23:33 +0200

    fix: web dev server NODE_ENV

--- frontend/web-dev-server.config.js
+++ frontend/web-dev-server.config.js
@@ -12,6 +12,18 @@ export default {
   open: true,
   preserveSymlinks: true,
   plugins: [
+    {
+      transform(context) {
+        if (context.response.type === 'application/javascript') {
+          return {
+            body: context.body.replace(
+              /process.env.NODE_ENV/g,
+              '\'development\''
+            ),
+          };
+        }
+      },
+    },
     legacyPlugin({
       polyfills: {
         // Manually imported in index.html file
